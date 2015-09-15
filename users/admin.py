3ee80ab07993cdc1f8b4b7823b101b118339b823
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-09-15 20:27:49 +0200

    Users can now be edited from django's admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User, UserForm
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    form = UserForm
+    list_display = ['login', 'profile_id', 'locked']
+    search_fields = ['login', 'profile_id']
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.forms import ModelForm, PasswordInput
+
+from passlib.hash import mysql41
 
 class User(models.Model):
     login = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
-    profile_id = models.IntegerField(editable=False, unique=True)
+    profile_id = models.IntegerField(unique=True)
     locked = models.BooleanField(default=False)
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['login', 'password', 'profile_id', 'locked']
+        widgets = {
+            'password': PasswordInput(),
+        }
+
+#     def clean_password(self):
+#         password = mysql41.encrypt(self.cleaned_data['password'])
+#         password = mysql41.encrypt('azerty123')
+#         password = "lol"
+#         return password
