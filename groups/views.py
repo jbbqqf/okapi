2fe028c2acf2e80581621b695d6b733d3258f151
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-09-18 00:24:21 +0200

    Only admins have write rights on groups routes

--- groups/views.py
+++ groups/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import detail_route, authentication_classes, permission_classes
 from rest_framework.response import Response
 
+from common.permissions import IsAdminOrReadOnly
 from groups.serializers import GroupSerializer, GroupUserSerializer
 from profiles.serializers import UserSerializer
 from groups.models import Group, GroupUser
 
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+@authentication_classes((SessionAuthentication, BasicAuthentication,))
+@permission_classes((IsAdminOrReadOnly,))
+class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-    # @detail_route()
-    # def users(self, request, pk=None):
-    #     group = self.get_object()
-    #     group_users = GroupUser.objects.filter(group=group.id)
+    @detail_route()
+    def users(self, request, pk=None):
+        group = self.get_object()
+        group_users = GroupUser.objects.filter(group=group.id)
 
-    #     users = []
-    #     for group_user in group_users:
-    #         users.append(User.objects.get(id=group_user.user.id))
+        users = []
+        for group_user in group_users:
+            users.append(User.objects.get(id=group_user.user.id))
 
-    #     serializer = UserSerializer(users, many=True)
-    #     return Response(serializer.data)
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
