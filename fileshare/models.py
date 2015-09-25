6b9c43e27b98777cb504eaf34cc6af39476370e9
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-09-25 15:51:21 +0200

    Initial fileshare models and base insertion

--- fileshare/models.py
+++ fileshare/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from groups.models import Group
 
-# Create your models here.
+class File(models.Model):
+    TYPE = [
+        ('f', 'file'),
+        ('d', 'directory'),
+    ]
+
+    parent = models.ForeignKey('self', null=True)
+    name = models.CharField(max_length=32)
+    creator = models.ForeignKey(User)
+    ownergroup = models.ForeignKey(Group, null=True)
+    type = models.CharField(max_length=1, choices=TYPE, default=TYPE[0][0])
+    deleted = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return name
