e0e4d98a982b82f97c92a7c8d453940881c2d34e
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-10-02 15:11:04 +0200

    Added a utility able to execute a pythonic shell command

---
+++ common/common.py
@@ -0,0 +1,8 @@
+# -*- include: utf-8 -*-
+
+from subprocess import Popen, PIPE
+
+def cmd(*args):
+    """Execute a shell command"""
+
+    return Popen(args, stdout=PIPE, stderr=PIPE).communicate()[0]
