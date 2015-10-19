b4cc3f3820760e9225b16e1df0e99d424dbf6787
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-10-20 00:17:40 +0200

    Admin views for themes application

--- themes/admin.py
+++ themes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from themes.models import UserInterface, UserInterfaceForm, Theme, ThemeForm, UserTheme, UserThemeForm
+
+class UserInterfaceAdmin(admin.ModelAdmin):
+    form = UserInterfaceForm
+    list_display = ['name', 'comment',]
+    search_fields = ['name', 'comment',]
+
+class ThemeAdmin(admin.ModelAdmin):
+    form = ThemeForm
+    list_display = ['ui', 'name', 'comment',]
+    search_fields = ['ui', 'name', 'comment',]
+
+class UserThemeAdmin(admin.ModelAdmin):
+    form = UserThemeForm
+    list_display = ['user', 'theme',]
+    search_fields = ['user', 'theme',]
+
+admin.site.register(UserInterface, UserInterfaceAdmin)
+admin.site.register(Theme, ThemeAdmin)
+admin.site.register(UserTheme, UserThemeAdmin)
