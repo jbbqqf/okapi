c1bc5d9239ab1bb25a12cae501f02571c7f8b46c
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-10-27 19:36:14 +0100

    Event model admin view

---
+++ news/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from news.models import Event, EventForm
+
+class EventAdmin(admin.ModelAdmin):
+    form = EventForm
+    list_display = ['author', 'title', 'description', 'link', 'created',
+                    'dday', 'visible',]
+    search_fields = ['author', 'title', 'description', 'link', 'dday',]
+
+admin.site.register(Event, EventAdmin)
