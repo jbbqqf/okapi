bc777799eb16dd2519530ca731a1ccf4f70db1b4
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-11-01 00:41:36 +0100

    Add dev and user documentation on grades application

--- grades/serializers.py
+++ grades/serializers.py
@@ -4,10 +4,26 @@ from urllib2 import HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build
 from rest_framework import serializers, exceptions
 
 class MyGradesSerializer(serializers.Serializer):
+    """
+    Serialize and validate wapiti platform login credentials. Wapiti and Okapi
+    credentials will often be the same, but it is not an obligation (ie.
+    graduates or people who didn't synchronized their password).
+
+    wapiti_ prefix on username/password reminds that you need to prefix your
+    username by `elv/` domain. We could auto-prefix username by `elv/`) but
+    this kind of automatisation is more likely to be handled on user interface
+    than on backend.
+    """
+
     wapiti_username = serializers.CharField(max_length=30)
     wapiti_password = serializers.CharField(max_length=128)
 
     def validate(self, attrs):
+        """
+        Validating those login datas is made by trying to open an accessible
+        wapiti url. If the page can be downloaded, it is validated.
+        """
+
         user = attrs.get('wapiti_username')
         passwd = attrs.get('wapiti_password')
 
@@ -25,6 +41,11 @@ class MyGradesSerializer(serializers.Serializer):
 
         try:
             opener.open(authelv)
+
+        # HTTPError is not very precise here. It could be wrong credentials as
+        # well as any other error (DNS resolving, unavailable resource...).
+        # TODO: try to handle errors more precisely even if wrong credentials
+        # appears to be the most common one.
         except HTTPError:
             error = 'Could not open {} with provided login data'.format(authelv)
             if not user.startswith('elv/'):
--- grades/views.py
+++ grades/views.py
@@ -13,6 +13,13 @@ from grades.serializers import MyGradesSerializer
 from grades.parsers import SchoolGradesParser, SchoolYearsParser
 
 def install_wapiti_opener(domain, user, passwd):
+    """
+    urllib2 can open pages with a default opener : no user, no password, just a
+    query on an http resource. But creating a custom opener allows to give auto
+    credentials and handle cookies, which we need due to the asp awful
+    application implementation.
+    """
+
     cookiejar = CookieJar()
 
     password_manager = HTTPPasswordMgrWithDefaultRealm()
@@ -23,6 +30,14 @@ def install_wapiti_opener(domain, user, passwd):
     install_opener(opener)
 
 def get_school_years_urls(url):
+    """
+    From default school grades page you have a list of all your years spent in
+    Telecom. This function list those years by reading available links via 
+    SchoolYearsParser and return a list of relative links.
+    Ex: ['/Commun/ens/adm/pf/pgs/etudiant/consulterResSco.aspx?anSco=19&' \\
+         'rangEtu=35 sur 83', link2, link3, ...]
+    """
+
     list_school_years_html = urlopen(url)
     
     list_school_years_data = ""
@@ -38,17 +53,37 @@ def get_school_years_urls(url):
     return school_years_urls
 
 def find_string_between(string, sub_first, sub_last):
+    """
+    Common function to find a substring surrounded by sub_first and sub_last.
+    sub_last can be set to None if for example you expect to isolate something
+    at the end of the string. In this case, the whole string after sub_first is
+    returned.
+
+    In case submitted data raises a ValueError, an empty string will be
+    returned instead.
+    """
+
     try:
         start = string.index(sub_first) + len(sub_first)
         if sub_last is not None:
             end = string.index(sub_last, start)
             return string[start:end]
+
         else:
             return string[start:]
+
     except ValueError:
         return ''
 
 def get_year_grades(year_grades_url):
+    """
+    Make use of SchoolGradesParser to parse all available data on your school
+    year grades pages and compact it in one big JSON structure.
+
+    Some data like the number of students and year id cannot be fetched on the
+    page so the url to access the page is used instead.
+    """
+
     year_grades_html = urlopen(year_grades_url)
 
     year_grades_data = ""
@@ -60,9 +95,14 @@ def get_year_grades(year_grades_url):
     year_grades = parser.grades
     parser.close()
 
-    # workaround to get number of students in your schoolyear as well as
-    # year `number`
-    # /Commun/ens/adm/pf/pgs/etudiant/consulterResSco.aspx?anSco=19&rangEtu=35 sur 83
+    # Workaround to get number of students in your schoolyear as well as
+    # year `id`. I don't know why it is not accessible on the page...
+    # Url example : '/Commun/ens/adm/pf/pgs/etudiant/consulterResSco.aspx?' \
+    # 'anSco=19&rangEtu=35 sur 83'
+    # Note : I don't know exactly where does year id come from, but I suspect
+    # it to be an incremented value since the year they released this grades
+    # app. In this case, it would be school year 1995-1996 which could be
+    # matching the first year of the first FIs.
     students = find_string_between(year_grades_url, 'sur ', None)
     year_id = find_string_between(year_grades_url, 'anSco=', '&rangEtu') 
 
@@ -73,7 +113,40 @@ def get_year_grades(year_grades_url):
 
 @permission_classes((AllowAny,))
 class MyGradesView(APIView):
+    """
+    === Access your school grades via webservice ! ===
+    
+    Anyone can access it since it does not provide anything else than what you
+    can read on your wapiti pages. That's also why you need to provide your
+    wapiti credentials each time your make a request here.
+    """
+
     def get(self, request, *args, **kwargs):
+        """
+        === Return your school grades in JSON ===
+
+        Since the amount of collected data can be huge depending on the number
+        of years the student spent in Telecom Lille ;), and as swagger does not
+        allow to format output with carriage return, an exemple of output can
+        be found here : http://wiki.bdetl.org/doku.php?id=okapi_grades.
+
+        You need to provide some JSON data in order to be able to access this
+        endpoint. Indeed, Wapiti and Okapi credentials do not necessarly share
+        the same login credentials. It will often be the same, but a student
+        can gradute or can forget to synchronize his old password...
+
+        Anyway, you need to provide wapiti_username and wapiti_password in
+        application/form-data JSON. Keep always in mind that to access a
+        resource on Wapiti you need to prefix your username by `elv/`. An error
+        will remind you that if your loggin fails and your forget this prefix.
+        It could be automaticaly added, but this kind of automation is more
+        likely to be handled on user interface than on backend.
+
+        Since you need to provide some application/form-data on a GET method,
+        which is unorthodox, django rest framework tools do not provide
+        built-in views to test the API... sorry for the inconvenience...
+        """
+
         serializer = MyGradesSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
