61eee6ac902a528078ed1da76ce4d0ce39895636
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-01-02 21:56:04 +0100

    Add /button/clears/latest/ as an easy access to the latest conqueror

--- button/views.py
+++ button/views.py
@@ -2,11 +2,12 @@
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.decorators import (
-    authentication_classes, permission_classes)
+    list_route, authentication_classes, permission_classes)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import (
     TokenAuthentication, SessionAuthentication)
 from rest_framework.filters import DjangoFilterBackend
+from rest_framework.response import Response
 
 from common.pagination import GamePagination
 from button.filters import ClearFilter
@@ -26,3 +27,13 @@ class ClearView(ReadOnlyModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filter_class = ClearFilter
     pagination_class = GamePagination
+
+    @list_route(methods=['GET'])
+    def latest(self, request, *args, **kwargs):
+        """
+        === The latest clear ===
+        """
+
+        latest = Clear.objects.latest('date')
+        serializer = self.get_serializer(latest)
+        return Response(serializer.data)
