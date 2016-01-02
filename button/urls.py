ed74c7dfda838605beebcd2bca5acd872c86b2bd
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-01-02 22:13:40 +0100

    Add /button/clear/ to clear the button

--- button/urls.py
+++ button/urls.py
@@ -2,11 +2,12 @@
 
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from button.views import ClearView
+from button.views import clear, ClearView
 
 router = DefaultRouter()
 router.register(r'clears', ClearView)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^clear/$', clear)
 ]
--- button/views.py
+++ button/views.py
@@ -2,17 +2,35 @@
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.decorators import (
-    list_route, authentication_classes, permission_classes)
+    api_view, list_route, authentication_classes, permission_classes)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import (
     TokenAuthentication, SessionAuthentication)
 from rest_framework.filters import DjangoFilterBackend
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 
 from common.pagination import GamePagination
 from button.filters import ClearFilter
 from button.models import Clear
 from button.serializers import ClearSerializer
+
+
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication, SessionAuthentication,))
+@permission_classes((IsAuthenticated,))
+def clear(request):
+    """
+    === Clear the button ! ===
+
+    Once per hour maximum.
+    """
+
+    clear = Clear(user=request.user)
+    clear.save()
+
+    serializer = ClearSerializer(clear)
+    return Response(serializer.data, status=HTTP_201_CREATED)
 
 
 @authentication_classes((TokenAuthentication, SessionAuthentication,))
